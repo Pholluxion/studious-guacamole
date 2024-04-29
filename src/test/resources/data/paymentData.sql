949d8f5f71186c21932b135fd6e3b71cf1d2f396
Carlos Peñaloza <carlos2172336@correo.uis.edu.co>
2024-04-28 22:27:13 -0500

    28/04/2024: fix payments test

--- src/test/resources/data/paymentData.sql
+++ src/test/resources/data/paymentData.sql
@@ -3,17 +3,13 @@ INSERT INTO payments (
     paid_amount,
     payment_time,
     spot_id,
-    fee_id,
-    date_created,
-    last_updated
+    fee_id
 ) VALUES (
     1500,
     56.08,
     '2023-09-02 16:30:00',
     1400,
-    1100,
-    '2023-09-02 16:30:00',
-    '2023-09-02 16:30:00'
+    1100
 );
 
 INSERT INTO payments (
@@ -21,15 +17,11 @@ INSERT INTO payments (
     paid_amount,
     payment_time,
     spot_id,
-    fee_id,
-    date_created,
-    last_updated
+    fee_id
 ) VALUES (
     1501,
     55.08,
     '2023-09-03 16:30:00',
     1401,
-    1101,
-    '2023-09-03 16:30:00',
-    '2023-09-03 16:30:00'
+    1101
 );
